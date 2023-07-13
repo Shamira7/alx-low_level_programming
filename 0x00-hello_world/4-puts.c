2b79358143bb3d2e8881e8aa9f366195685c3e73
Shamira7 <shamirah7@outlook.com>
2023-07-13 14:14:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this main function is to print the stated text
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
