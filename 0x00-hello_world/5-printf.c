a1bfead5896b07c4249a9ce93782a10baed6660b
Shamira7 <shamirah7@outlook.com>
2023-07-13 14:06:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this main function is to print the stated text
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
