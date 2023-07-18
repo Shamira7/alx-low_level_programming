a9f53f486f9f8bcc0600841010d9e1a1f8757d85
Shamira7 <shamirah7@outlook.com>
2023-07-18 07:53:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase.
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
