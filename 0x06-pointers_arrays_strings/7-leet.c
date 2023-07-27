886d95c61e54e58c41a507b97ed19c4c90a38481
Shamira7 <shamirah7@outlook.com>
2023-07-27 07:22:41 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - Change vowels to numbers.
+ * @s: analized string.
+ * Return: string with all vowels changed.
+ */
+
+char *leet(char *s)
+
+{
+	char a[] = "aeotlAEOTL";
+	char n[] = "4307143071";
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	if (*(s + i) == a[j])
+	{
+	*(s + i) = n[j];
+	}
+	}
+	i++;
+	}
+	return (s);
+}
