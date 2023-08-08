963f968eb28864cce638020ec514c4f17918496b
Shamira7 <shamirah7@outlook.com>
2023-08-08 06:24:33 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees two dimensional grid
+ * @grid: two dimensional grid
+ * @height: height dimension of grid
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
