9919927bcb23ea638b9f9d7065a4b31d45087d7c
Shamira7 <shamirah7@outlook.com>
2023-09-29 16:11:16 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: number of lines in argv argument
+ * @argv: argument vector
+ * Return: 0 when successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
